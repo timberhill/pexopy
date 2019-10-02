deacb269bcc1737c12e7e0d19ffd28d224682d4a
Max Timberhill <max.lisogorski@gmail.com>
2019-10-02 16:51:41 +0100

    updated Pexo setup

--- pexopy.py
+++ pexopy.py
@@ -6,25 +6,69 @@ import subprocess
 
 class Pexo(object):
     """
-    PEXO wrapper class
+    A Python wrapper for PEXO - https://github.com/phillippro/pexo/
+
+
     """
     def __init__(self):
-        self.R       = subprocess.check_output("which R", shell=True).decode("ascii").strip()
-        self.Rscript = subprocess.check_output("which Rscript", shell=True).decode("ascii").strip()
-        self.pexodir = subprocess.check_output("echo $PEXODIR", shell=True).decode("ascii").strip()
-
-        self.pexo_main = "{}/{}".format(self.pexodir, "code/binary_test.R").replace("//", "/")
+        self.setup()
 
         self.cwd = os.getcwd() # current directory
-        
+
+
+    def setup(self, Rscript=None, pexodir=None):
+        """
+        Set up the wrapper.
+
+        Rscript, <str>: path to Rscript, optional
+
+        pexodir, <str>: path to PEXO repository, optional if $PEXODIR environment variable is installed.
+        """
+        # Find and validate Rscript
+
+        if Rscript is None: # find Rscript
+            rc = subprocess.call(['which', 'Rscript'])
+            if rc == 0: # 'which' found the path
+                self.Rscript = subprocess.check_output("which Rscript", shell=True).decode("ascii").strip()
+            else: # 'which' returned an error
+                raise OSError("Could not find Rscript. Make sure it's installed correctly or specify the path to it in Pexo.setup(Rscript=)")
+        else: # use the path provided by user
+            if os.path.exists(Rscript) and Rscript.endswith("Rscript"): # success
+                self.Rscript = Rscript
+            else:
+                raise OSError("Specified Rscript path is not valid.")
+
+        # Find and validate PEXO directory path
+
+        if pexodir is None:
+            if "PEXODIR" in os.environ: # check the PEXODIR environment variable
+                self.pexodir = os.getenv("PEXODIR")
+            else: # PEXODIR is not set
+                raise OSError("PEXODIR environment variable is not set. Consider setting it or specifying it in Pexo.setup(pexodir=)")
+        else:
+            self.pexodir = pexodir
+
+        # make sure this folder is legit
+        if \
+            not os.path.exists(self.pexodir) or \ 
+            not os.path.exists(os.path.join(self.pexodir, "code")) or \
+            not os.path.isfile(os.path.join(self.pexodir, "code/pexo.R")):
+            raise OSError("The PEXO directory specified is not valid.")
+            
+
+        self.pexo_main = os.path.join(self.pexodir, "code/pexo.R")
+
 
     def _get_last_output(self):
+        """
+        Get
+        """
         raise NotImplementedError("Pexo._get_last_output() is not yet implemented.")
 
 
     def run(self, binary_model="DDGR", observatory=None, astrometry=None, binary=None):
         """
-        run PEXO
+        Run PEXO
         """
         self._validate_parameters(binary_model, observatory, astrometry, binary)
 
@@ -64,3 +108,5 @@ class Pexo(object):
 
         if not isinstance(observatory, str):
             raise NotImplementedError('validation is not implemented yet')
+
+Pexo()
